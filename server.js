36871ad02a8975e8d33434ef225d0581f979a294
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2024-10-07 22:23:56 -0300

    feat(teste): testando estudo + husck + prettier

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-import http from "http"; 
+import http from 'http';
 
 const PORT = 3000;
 
 const rotas = {
-  "/": "Curso de Node.js"
+  '/': 'Curso de Node.js',
+  '/livros': 'Entrei na rota livros',
+  '/autores': 'Entrei na rota autores',
 };
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
-  console.log("servidor escutando!");
+  console.log('servidor escutando!');
 });
