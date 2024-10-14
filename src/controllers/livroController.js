12d372b53dbdb1185e3eae270fd9f6ad71bb0a72
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2024-10-14 00:11:02 -0300

    feat: buscando livro via query param

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -63,6 +63,18 @@ class LivroController {
       logHelper.show('ERRO AO TENTAR DELETAR LIVRO', error);
     }
   }
+
+  static async buscarLivroPorAutor(req, res) {
+    try {
+      const editora = req.query.editora;
+      const regex = new RegExp(editora, 'i');
+      const livro = await livros.find({ editora: regex });
+      res.status(200).json(livro);
+    } catch (error) {
+      res.status(500).send(error.message);
+      logHelper.show('ERRO AO TENTAR LISTAR LIVRO POR EDITORA', error);
+    }
+  }
 }
 
 export default LivroController;
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,6 +4,11 @@ import LivroController from '../controllers/livroController.js';
 const routes = express.Router();
 
 routes.get('/', LivroController.listar);
+/**
+ * o /busca precisa ficar acima do /:id quando tem 2 gets pois o express vai resolver o primeiro que achar,
+ * se o /busca ficasse depois do /:id ele iria pensar que o /busca é um id, no express as rotas de maior complexidade sempre devem ficar no topo.
+ */
+routes.get('/busca', LivroController.buscarLivroPorAutor);
 routes.get('/:id', LivroController.buscarPorId);
 routes.post('/', LivroController.cadastrar);
 routes.put('/:id', LivroController.atualizar);
