0700266ac6160eb1ab26692a540568833f30d93b
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2024-10-22 23:24:06 -0300

    feat: adicionando validações e mensagens de erros para o usuário em caso de bad request

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,10 +3,32 @@ import mongoose from 'mongoose';
 const livroSchema = new mongoose.Schema(
   {
     id: { type: mongoose.Schema.Types.ObjectId },
-    titulo: { type: String, required: [true, 'Campo obrigatório.'] },
-    editora: { type: String, required: [true, 'Campo obrigatório.'] },
+    titulo: {
+      // Define o tipo do campo, deixa ele obrigatorio e da uma mensagem de erro caso envie fora do padrão.
+      type: String,
+      required: [true, 'Campo obrigatório.'],
+    },
+    editora: {
+      type: String,
+      required: [true, 'Campo obrigatório.'],
+      enum: {
+        values: ['Casa do Código', 'Branca de Neve'],
+        message: 'editora {VALUE} não permitida.',
+      },
+    }, // Limito as entradas desse campo ao que é colocado no array, algo
     preco: { type: Number, required: [true, 'Campo obrigatório.'] },
-    paginas: { type: Number, required: [true, 'Campo obrigatório.'] },
+    paginas: {
+      type: Number,
+      min: [
+        10,
+        'O Número mínimo de páginas é 10, O valor fornecido foi {VALUE}.',
+      ], // Define o tipo do campo, deixa ele como minimo de 10 e da uma mensagem de erro caso envie fora do padrão.
+      max: [
+        5000,
+        'O Número máximo de páginas é 5000. O valor fornecido foi {VALUE}',
+      ],
+      required: [true, 'Campo obrigatório.'],
+    },
     autor: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'autores',
